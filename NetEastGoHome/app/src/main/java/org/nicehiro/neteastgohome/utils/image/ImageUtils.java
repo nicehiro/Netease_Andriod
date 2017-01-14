4ddc7263a9b4a4b15a68e6a8900b48b0c2eedd62
hiro <wfy11235813@gmail.com>
2017-01-14 19:03:43 +0800

    backup

---
+++ NetEastGoHome/app/src/main/java/org/nicehiro/neteastgohome/utils/image/ImageUtils.java
@@ -0,0 +1,65 @@
+package org.nicehiro.neteastgohome.utils.image;
+
+import android.widget.ImageView;
+
+import com.bumptech.glide.load.resource.bitmap.BitmapTransformation;
+
+/**
+ * Created by hiro on 1/14/17.
+ */
+
+public class ImageUtils {
+
+    // 针对网络图片
+    public static void setUrl(ImageView imageView, String url) {
+
+    }
+
+    public static void setUrl(ImageView imageView, String url, int width, int height) {
+
+    }
+
+    public static void setUrl(ImageView imageView, String url, int radiusDp) {
+
+    }
+
+    public static void setUrl(ImageView imageView, String url, int width, int height, int radiusDp) {
+
+    }
+
+    public static void setCircleUrl(ImageView imageView, String url) {
+
+    }
+
+    public static void setCirclrUrl(ImageView imageView, String url, int width, int height) {
+
+    }
+
+    public static void setUrl(ImageView imageView, String url, int width, int height, BitmapTransformation transformation) {
+
+    }
+
+    // 针对本地资源图片
+    public static void setResId(ImageView imageView, int resId) {
+
+    }
+
+    public static void setResId(ImageView imageView, int resId, int radiusDp) {
+
+    }
+
+    public static void setCircleResId(ImageView imageView, int resId) {
+
+    }
+
+    public static void setResId(ImageView imageView, int resId, BitmapTransformation transformation) {
+
+    }
+
+    //针对 sdcard 图片
+    public static void setFilePath(ImageView imageView, String filePath) {
+
+    }
+
+    public static void setFilePah(ImageView imageView, String filePath, int radiusDp)
+}
